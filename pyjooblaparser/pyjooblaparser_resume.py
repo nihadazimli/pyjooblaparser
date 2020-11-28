fc14bb3377f24e57baf24d986b87feb80dfed06d
your_username <vatan@vatan-MacBook-Pro.local>
2020-11-28 17:36:03 +0400

    Bugs In experience years and educattion years were fixed

--- pyjooblaparser/pyjooblaparser_resume.py
+++ pyjooblaparser/pyjooblaparser_resume.py
@@ -20,6 +20,7 @@ class ResumeParser(object):
             'education':None,
             'experience':None,
             'total_experience':None
+            #'edu2':None
         }
         self.__resume = resume
 
@@ -52,12 +53,16 @@ class ResumeParser(object):
 
         # personal_information_seperator_regex = config.PERSONAL_INFORMATION_SEPERATOR
         # personal_information_raw_text = re.split(personal_information_seperator_regex, self.__text_raw.lower())
-        this = self.__nlp
+        #this = self.__nlp
         entities = utils.extract_entity_sections_grad(self.__text_raw)
-        self.__details['education'] = utils.extract_education ([sent.string.strip() for sent in this.sents])
+        #self.__details['education'] = utils.extract_education ([sent.string.strip() for sent in this.sents])
         self.__details['email'] = utils.extract_email(self.__text_raw)
         self.__details['mobile_number'] = utils.extract_number(self.__text_raw)
         self.__details['skills'] = utils.extract_skills(self.__nlp,self.__noun_chunks,None)
         self.__details['experience'] = utils.extract_experience(entities['experience'])
+        nlp = spacy.load('en_core_web_sm')
+        e = nlp('\n'.join(entities['education']))
+        self.__details['education'] = utils.extract_education([sent.string.strip() for sent in e.sents])
         self.__details['total_experience'] = utils.get_total_experience(entities['experience'])
+        #self.__details['edu2'] = utils.extract_education2(entities['education'])
         return self
--- pyjooblaparser/utils.py
+++ pyjooblaparser/utils.py
@@ -23,7 +23,7 @@ MONTHS_SHORT = r'(jan)|(feb)|(mar)|(apr)|(may)|(jun)|(jul)|(aug)|(sep)|(oct)|(no
 MONTHS_LONG = r'(january)|(february)|(march)|(april)|(may)|(june)|(july)|(august)|(september)|(october)|(november)|(december)'
 MONTH = r'(' + MONTHS_SHORT + r'|' + MONTHS_LONG + r')'
 EDUCATION = ['UNIVERSITY','B.E.', 'B.E',"B.Sc", 'ME', 'M.E', 'M.E.', 'MS', 'M.S', 'BTECH', 'MTECH',
-                    'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII','PHD','BS','HIGH SCHOOL','COLLEGE','SCHOOL'
+                    'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII','PHD','BS','HIGH SCHOOL','COLLEGE','SCHOOL','BSC','MSC'
                     ]
 
 RESUME_SECTIONS = [
@@ -239,6 +239,7 @@ def extract_education(nlp_text):
                 education.append((key,biggest))
         else:
             education.append(key)
+
         #print(year.string)
         # if year:
         #     education.append((key, ''.join(year.group(0))))
@@ -246,6 +247,27 @@ def extract_education(nlp_text):
         #     education.append(key)
     return education
 
+# def extract_education2(education_list):
+#     edu = {}
+#     for line in education_list:
+#         line = re.sub(r'[?|$|.|!|,]', r'',line)
+#         words = line.split()
+#         count = 0
+#         for word in words:
+#             count = count + 1
+#             if word.upper() in EDUCATION and word not in STOPWORDS:
+#                 if (word.lower() == 'university'):
+#                     line = words[count-2] + " " + word
+#                 edu[word] = line
+#
+#     return edu
+
+        # if line.upper() in EDUCATION and line not in STOPWORDS:
+        #     if (lnie.lower() == 'university'):
+        #         line = splitted[count - 1] + " " + tex
+        #     edu[tex] = text + nlp_text[index]
+        #
+        # count = count + 1
 
 
 def extract_entity_sections_grad(text):
@@ -255,9 +277,36 @@ def extract_entity_sections_grad(text):
     :param text: Raw text of resume
     :return: dictionary of entities
     '''
+    text = re.sub("/"," ",text)
     text_split = [i.strip() for i in text.split('\n')]
+    text_split2 = text_split
     # sections_in_resume = [i for i in text_split if i.lower() in sections]
     entities = {}
+    count = 0
+    #print(text_split2)
+    for i in text_split2:
+        count = count + 1
+        #print("SQQQ",i)
+        experience = re.search(
+        r'(?P<fmonth>\w+.\d\d\d\d)\s*(\D|to)\s*(?P<smonth>\w+.\d\d\d\d|present)',
+        i,
+        re.I
+        )
+        if experience:
+            try:
+                if text_split2[count - 1] != '' and text_split2[count - 3] != '':
+                    text_split2[count-3] = text_split2[count - 3]+ " " + text_split2[count-1]
+                    text_split2.pop(count - 1)
+                #print(text_split2)
+            except IndexError:
+                print("INDEX")
+                # if text_split2[count - 1] == '':
+                #     text_split2 = text_split2[count - 1] + text_split2[count]
+
+
+
+    text_split = text_split2
+    #print("THIS",text_split)
     key = False
     for phrase in text_split:
         if len(phrase) == 1:
@@ -273,6 +322,8 @@ def extract_entity_sections_grad(text):
             key = p_key
         elif key and phrase.strip():
             entities[key].append(phrase)
+        count = count + 1
+
 
     # entity_key = False
     # for entity in entities.keys():
